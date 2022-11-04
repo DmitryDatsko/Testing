fd9a833166d18afb439cb4317105c0a274ad731d
DmitryDatsko <dmitrydatsko.of@gmail.com>
2022-11-04 15:25:54 +0200

    full commit

---
+++ SimpleApp/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace SimpleApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal? Price { get; set; }
+        //public static Product[] GetProducts()
+        //{
+        //    Product kayak = new()
+        //    {
+        //        Name = "Kayak",
+        //        Price = 275M
+        //    };
+        //    Product lifejacket = new()
+        //    {
+        //        Name = "Lifejacket",
+        //        Price = 48.95M
+        //    };
+        //    return new Product[] { kayak, lifejacket };
+        //}
+    }
+    public class ProductDataSource : IDataSource
+    {
+        public IEnumerable<Product> Products =>
+            new Product[]
+            {
+                new Product {Name="Kayak", Price = 275M},
+                new Product {Name="Kayak", Price = 48.95M},
+            };
+    }
+}
